958ac5e3f5647b81acbd776823f27a5cf6aa5c16
Tobias Wilfert <36408720+tobias-wilfert@users.noreply.github.com>
2022-07-01 12:31:19 +0000

    Prove of concept

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import csv
+import random
 
 app = Flask(__name__)
 
+with open('col3.csv') as csvfile:
+    data = list(csv.reader(csvfile))
+
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    # Get random entry from data
+    item = random.choice(data)
+    return render_template("index.html", year=item[5], link=item[6])
 
 app.run(host='0.0.0.0', port=81)
