2628d4182a159fcaa35ba6ffff577c55f3614f60
Tobias Wilfert <36408720+tobias-wilfert@users.noreply.github.com>
2022-07-01 18:13:15 +0000

    Added an endpoint that contains only paintings

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ app = Flask(__name__)
 with open('col3.csv') as csvfile:
     data = list(csv.reader(csvfile))
 
+with open('paintings.csv') as csvfile:
+    paintings = list(csv.reader(csvfile))
+
 @app.route('/')
 def index():
     # Get random entry from data
     item = random.choice(data)
+    return render_template("index.html", year=item[5], link=item[6])
+
+@app.route('/paintings')
+def paintings_endpoint():
+    # Get random entry from data
+    item = random.choice(paintings)
     return render_template("index.html", year=item[5], link=item[6])
 
 app.run(host='0.0.0.0', port=81)
